da5b668e7f670ceb15db96803347527b424a7442
Sergio Sanchez Robles <sergioj.sanchezr@gmail.com>
2016-06-25 20:58:53 -0500

    Codigo para Segundo arff generado

--- extract_data_credit_card.rb
+++ extract_data_credit_card.rb
@@ -1,5 +1,6 @@
 
 outputfile = File.open("credit_card_clients.arff", 'w');
+outputfileTest = File.open("credit_card_clients_test.arff", 'w');
 linecount = 0;
 File.readlines("default of credit card clients.csv").each do |line|
    unless linecount == 0
@@ -7,16 +8,29 @@ File.readlines("default of credit card clients.csv").each do |line|
       tokens = tokens.drop(1)
       if linecount == 1
          outputfile.write("@relation credit-card-clients\n")
+         outputfileTest.write("@relation credit-card-clients\n")
          tokens.each do |attr|
             outputfile.write("@attribute #{attr.strip.gsub(/\s/, '_')} numeric \n")
+            outputfileTest.write("@attribute #{attr.strip.gsub(/\s/, '_')} numeric \n")
          end
          outputfile.write("\n@data\n")
+         outputfileTest.write("\n@data\n")
       else
-         outputfile.write("#{tokens.join(",")}\n")
+         if linecount < 350
+            outputfile.write("#{tokens.join(",")}\n")
+         elsif linecount < 370
+            tokens[0] = '?'
+            outputfileTest.write("#{tokens.join(",")}\n")
+         else
+            break
+         end
       end
    end
    linecount = linecount + 1
-   break if linecount > 350
 end
 
 outputfile.close
+outputfileTest.close
+
+puts "Credit card .arff fiels created successfully"
+exit 0
